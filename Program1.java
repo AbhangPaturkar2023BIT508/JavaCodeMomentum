f1c49bff01874a65f2932c770af7aa992319c0a5
Abhang Paturkar <2023bit508@sggs.ac.in>
2024-01-12 23:29:55 +0530

    Integer Leading '0' Program

---
+++ Program1.java
@@ -0,0 +1,12 @@
+class Program1 {
+    public static void main(String[] args) {
+        int x = 0103; // Leading '0' before numeric literal indicates Octal Value
+        System.out.println("x = " + x); // x = 67
+
+        int y = 103;
+        System.out.println("y = " + y); // y = 103
+
+        int z = 0115;
+        System.out.println("z = " + z); // z = 77
+    }
+}
