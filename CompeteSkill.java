e77d0467450f0bcd939dbe9684a5d498dea238d5
Abhang Paturkar <155937375+AbhangPaturkar2023BIT508@users.noreply.github.com>
2024-02-09 23:44:25 +0530

    CompeteSkills

---
+++ CompeteSkill.java
@@ -0,0 +1,20 @@
+class Solution {
+
+    public void scores(long a[], long b[]) {
+        // store the answer as
+        // GFG.ca=3;
+        // GFG.cb=5;
+        // Your code goes here
+        int ca = 0, cb = 0;
+        for(int i = 0; i<a.length;i++){
+            if(a[i] > b[i]){
+                ca++;
+            }
+            if(a[i] < b[i]){
+                cb++;
+            }
+        }
+        GFG.ca = ca;
+        GFG.cb = cb;
+    }
+}
