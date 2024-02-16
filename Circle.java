83db0cc0e086e8975d2f50993feae0ac353515a3
Abhang Paturkar <155937375+AbhangPaturkar2023BIT508@users.noreply.github.com>
2024-02-16 21:23:59 +0530

    TwoCircleTouchEachOther

---
+++ Circle.java
@@ -0,0 +1,8 @@
+class Solution {
+    int circleTouch(int X1, int Y1, int R1, int X2, int Y2, int R2) {
+        int distanceBetweenCentresSquared =
+            (X2 - X1) * (X2 - X1) + (Y2 - Y1) * (Y2 - Y1);
+        int sumOfRadiiSquared = (R2 + R1) * (R2 + R1);
+        return ((distanceBetweenCentresSquared <= sumOfRadiiSquared)?1:0);
+    }
+}
