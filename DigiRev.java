031fe60982aa548b4e1ffb8d9eb7e2d91000d886
Abhang Paturkar <155937375+AbhangPaturkar2023BIT508@users.noreply.github.com>
2024-03-08 23:28:39 +0530

    Reverse digit

---
+++ DigiRev.java
@@ -0,0 +1,15 @@
+class Solution
+{
+    public long reverse_digit(long n)
+    {
+        // Code here
+        long rev=0l;
+        while(n>0){
+            long digit = n % 10;
+            rev = (rev*10)+digit;
+            n/=10;
+        }
+        
+        return rev;
+    }
+}
