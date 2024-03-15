b811f75761ca2291f2dc68dd8c7a4f9a3a167847
Abhang Paturkar <abhangpaturkar0709@gmail.com>
2024-03-15 12:25:56 +0530

    CopyArray

---
+++ OnlyStatic.java
@@ -0,0 +1,14 @@
+public class OnlyStatic extends javafx.application.Application{
+    static{
+        System.out.println("hello");
+    }
+    public static void main() {
+        
+    }
+}
+
+class Sggs{
+    Sggs(){
+        System.out.println("Sggs() consturctor");
+    }
+}
