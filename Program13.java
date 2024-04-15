c9098550055d6d7e61cb246b46813b6bbebfcfda
Abhang Paturkar <155937375+AbhangPaturkar2023BIT508@users.noreply.github.com>
2024-04-15 22:51:52 +0530

    code

--- Program13.java
+++ Program13.java
@@ -1,3 +1,20 @@
-public class Program13 {
-    
+public class Main {
+    public static void main(String[] args) {
+        PrintNumber p1 = new PrintNumber(10);
+        p1.display();
+    }
+}
+
+class PrintNumber {
+    private int N;
+
+    public PrintNumber(int n) {
+        N = n;
+    }
+
+    public void display() {
+        for (int i = 1; i <= N; i++) {
+            System.out.print(i + " ");
+        }
+    }
 }
